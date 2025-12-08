7898e124386e5f1040522e9957b91fab4376334d
sebakai <seb.wohlhauser@yahoo.com>
2025-12-08 19:40:43 +0100

    add storage

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_storage_account" "storageaccount" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_account" "storageaccount2" {
+resource "azurerm_storage_account" "storageaccount3" {
   name                     = "stosebaka75"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
